7bdc862cbb26f39a0d571316bfae130033ab1820
BanditBloodwyn <kevin@eichenbergs.info>
2023-03-08 11:09:48 +0100

    StrategicCameraSettings

---
+++ Assets/_Project/_Scripts/Gameplay/Controls/Camera/StrategicCameraSettings.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace Assets._Project._Scripts.Gameplay.Controls.Camera
+{
+    [CreateAssetMenu(fileName = "StrategicCameraSettings", menuName = "ScriptableObjects/Settings/Camera/Strategic Camera Settings")]
+    public class StrategicCameraSettings : ScriptableObject
+    {
+        [Range(1.0f, 10.0f)] public float PanningSpeed = 20.0f;
+        [Range(0.01f, 1.0f)] public float RotationSpeed = 0.1f;
+        [Range(0.1f, 10.0f)] public float ZoomSpeed = 2.0f;
+        [Range(1.0f, 50.0f)] public float ZoomDamping = 20.0f;
+    }
+}
